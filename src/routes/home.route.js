d4d6522ef20275c3c8f9007cd26cff991a303ce3
ExoCryptic <94581934+ExoCryptic@users.noreply.github.com>
2025-10-23 13:26:12 +0700

    Update home.route.js
    Moved load categories to here

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -6,17 +6,24 @@ import homeController from "../controller/home.controller.js";
 // CATEGORIES LOAD
 // ==========================
 router.use(async function (req, res, next) {
-  const list = await categoryModel.getCategoriesL2_L1();
-  res.locals.globalCategories = list;
-
-  next();
+  try {
+    const list = await categoryModel.getCategoriesL2_L1();
+    res.locals.globalCategories = list;
+    next();
+  } catch (error) {
+    // Lỗi sẽ được hiển thị ở đây!
+    console.error("LỖI NGHIÊM TRỌNG KHI TẢI CATEGORIES:", error);
+    // Chuyển lỗi đến trang "Something went wrong" một cách tường minh
+    next(error); 
+  }
 });
-// ==========================
+
 router.get("/", async (req, res) => {
     try {
         const categories = await categoryModel.index(req, res);
         res.locals.categories = categories;
-        res.render("home", { categories });
+        const data = await homeController.index(req, res);
+        res.render("home",  data );
     } catch (error) {
         console.error('Error fetching categories:', error);
         res.status(500).render('error');
