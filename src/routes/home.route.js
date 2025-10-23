caeb289df14e4854af149f27ef75bb3a735fb2c8
ExoCryptic <94581934+ExoCryptic@users.noreply.github.com>
2025-10-23 13:12:08 +0700

    Update home.route.js
    Load categories

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -2,6 +2,16 @@ import express from "express";
 const router = express.Router();
 import categoryModel from "../models/category.model.js";
 import homeController from "../controller/home.controller.js";
+// ==========================
+// CATEGORIES LOAD
+// ==========================
+router.use(async function (req, res, next) {
+  const list = await categoryModel.getCategoriesL2_L1();
+  res.locals.globalCategories = list;
+
+  next();
+});
+// ==========================
 router.get("/", async (req, res) => {
     try {
         const categories = await categoryModel.index(req, res);
