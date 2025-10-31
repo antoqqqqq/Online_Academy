57300cfb5ab196b79100a155dc8f668b97c6fdbb
domanh10 <161590352+domanh10@users.noreply.github.com>
2025-10-31 16:58:08 +0700

    2.8-Do-Manh-Test

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,15 +8,17 @@ dotenv.config();
 
 // Local login
 passport.use(new LocalStrategy(async (email, password, done) => {
-  const user = await accountModel.findByEmail(email);
-  if (!user) return done(null, false, { message: 'User not found' });
-  console.log(user);
+  try {
+    const user = await accountModel.findByEmail(email);
+    if (!user) return done(null, false, { message: 'User not found' });
 
-  const match = await bcrypt.compareSync(password, user.password);
-  console.log(match);
-  if (!match) return done(null, false, { message: 'Wrong password' });
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return done(null, false, { message: 'Wrong password' });
 
-  return done(null, user);
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
 }));
 
 // Google login
